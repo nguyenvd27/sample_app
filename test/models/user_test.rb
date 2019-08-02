12d6cb157d5fe8a5a2a5bbe33320a4101345d1bf
nguyenvd27 <ducnguyen272727@gmail.com>
2019-08-02 22:48:03 +0700

    Add user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -20,10 +20,10 @@ class UserTest < ActiveSupport::TestCase
   	assert_not @user.valid?
   end
 
-  test "name should not be too long" do
-  	@user.name = "a"*51
-  	assert_not @user.valid?
-  end
+  # test "name should not be too long" do
+  # 	@user.name = "a"*51
+  # 	assert_not @user.valid?
+  # end
 
   test "email should not be too long" do
   	@user.email = "a"*244 + "@example.com"
@@ -66,5 +66,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?('')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
