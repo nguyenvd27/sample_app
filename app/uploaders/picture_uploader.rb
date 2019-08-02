4e8aa68fa61b07929f77e46df86e83783bc237e0
nguyenvd27 <ducnguyen272727@gmail.com>
2019-08-02 23:09:15 +0700

    Fix produce env

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,14 +6,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # chua co AWS nen chua up anh len AWS dc
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
   
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
