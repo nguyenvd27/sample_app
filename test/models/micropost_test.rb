066e33f4d08da8de768b3b19897ea6ff3132bb53
nguyenvd27 <ducnguyen272727@gmail.com>
2019-08-03 00:32:33 +0700

    length micropost: 14000

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -22,8 +22,8 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
-  test "content should be at most 140 characters" do
-    @micropost.content = "a" * 141
+  test "content should be at most 14000 characters" do
+    @micropost.content = "a" * 14001
     assert_not @micropost.valid?
   end
 
